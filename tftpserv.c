c2583846c832d3cec03bd3221c51dd8067dbab44
Schmaj <jwschmalz@outlook.com>
2019-09-28 20:36:35 -0400

    MAYBE

--- tftpserv.c
+++ tftpserv.c
@@ -238,15 +238,18 @@ void childFunction(unsigned int fd, char* buffer, struct sockaddr* addr, socklen
 			bzero(data, MAX_PACKET);
 			memcpy(data, &buffer[4], MAX_PACKET-4);
 			data[MAX_PACKET] = '\0';
-			write(file_d, data, strlen(data));
+			if (size != MAX_PACKET){ //END OF TRANSMISSION
+				write(file_d, data, size-5);
+				((short*)ack)[0] = htons(4);
+				((short*)ack)[1] = htons(blockcount);
+				sendto(fd, ack, 4, 0, addr, sizeof(struct sockaddr_in));
+				close(file_d);
+				return;
+			}	
+			write(file_d, data, size-4);
 			((short*)ack)[0] = htons(4);
 			((short*)ack)[1] = htons(blockcount);
 			sendto(fd, ack, 4, 0, addr, sizeof(struct sockaddr_in));
-			if (size != MAX_PACKET){ //END OF TRANSMISSION
-				close(file_d);
-				return;
-			}		
-
 		}
 
 	}
