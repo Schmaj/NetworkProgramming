881c7c565b6df22a761f464ad88f00d601a50dc7
Schmaj <jwschmalz@outlook.com>
2019-10-10 18:01:32 -0400

    Basics

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -10,6 +10,8 @@
 #include <fcntl.h>
 #include <pthread.h>
 
+#define NUM_CHILD 5
+
 struct addStruct
 {
 	int x;
@@ -17,10 +19,15 @@ struct addStruct
 };
 
 void * add(void * arg){
-	struct addStruct nums = (struct addStruct)arg;
-	return (void*)(num.x + nums.y); 
+	struct addStruct nums = *(struct addStruct *)arg;
+	return (void*)(nums.x + nums.y); 
 }
 
 int main(int argc, char ** argv){
-	
+	pthread_t children[NUM_CHILD * (NUM_CHILD - 1)];
+	for (unsigned long i = 0; i < NUM_CHILD; ++i){
+		for (unsigned long j = 0; j < NUM_CHILD; ++j){
+			printf("Main starting thread add() for [%ld + %ld]\n", i + 1, j + 1);
+		}
+	}
 }
