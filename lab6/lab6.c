850262dd9cc45d1d17927e6f2a9b196bd0556892
Schmaj <jwschmalz@outlook.com>
2019-10-31 17:59:25 -0400

    Added example

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <sys/types.h>
-
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/select.h>
-
 #include <signal.h>
 #include <fcntl.h>
-
 #include <errno.h>
-
 #include <ctype.h>
+#include <netdb.h>
 
 int main() {
 	int status;
