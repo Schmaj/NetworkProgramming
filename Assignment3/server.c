8d9f18232ed9482f883b5c0621622ce8e3fa42f4
Schmaj <jwschmalz@outlook.com>
2019-11-06 19:24:35 -0500

    Added Assignment 3

---
+++ Assignment3/server.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <ctype.h>
+#include <netdb.h>
+
+#include <arpa/inet.h>
+
+int main(int argc, char * argv[]) {
+	return 0;
+}
