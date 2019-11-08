25e752bc37f3335c7bef654583488542c280bd93
Schmaj <jwschmalz@outlook.com>
2019-11-07 19:24:06 -0500

    Parser

--- Assignment3/ParseTest.c
+++ Assignment3/ParseTest.c
@@ -16,43 +16,40 @@
 
 #include <arpa/inet.h>
 
-
 struct hoplist{
 	char* id;
 	struct hoplist* next;
-}
+};
 
 struct message{
+	char* messageType;
 	char* originID;
 	char* nextID;
 	char* destinationID;
 	int hopLeng;
 	struct hoplist* hoplst;
-}
+};
 
-struct message * parseMsg(char * msg){ 
-	char * keyword = strtok(msg, " ");
-		
+struct message * parseMsg(char * msg){
+	char * cpy = calloc(strlen(msg)+1, sizeof(char));
+	strcpy(cpy, msg);
+
+	char * keyword = strtok(cpy, " ");
+	if (strcmp(keyword, "DATAMESSAGE") == 0){
+		printf("DATAMESSAGE\n");
+		free(cpy);
+	} else if (strcmp(keyword, "THERE") == 0){
+		printf("THERE\n");
+		free(cpy);
+	}
+
+	struct message * empty = calloc(1, sizeof(struct message));
+	return empty;
 }
 
 int main(int argc, char * argv[]) {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	struct message * test = parseMsg("DATAMESSAGE HERE");
+	free(test);
 	return 0;
 }
-
-/*
-
-select() to read from stdin and any tcp connections
-
-sendMsg()
-
-recvMsg()
-
-interactWithConsole() // interpreting commands
-
-
-
-msgToStr()
-
-
-
-*/
