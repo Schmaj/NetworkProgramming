a49751c3c3870f7900753d4e03ede5ca1d10e1e9
Andrew Conley <conley@localhost.localdomain>
2019-12-07 18:53:12 -0500

    ?

--- Assignment4/hw4.py
+++ Assignment4/hw4.py
@@ -95,7 +95,7 @@ def rpcFindNode(recipient, requestedId, meNode):
 	remote_addr = recipient.address
 	remote_port = recipient.port
 	with grpc.insecure_channel(remote_addr + ':' + str(remote_port)) as channel:
-		idKey = csci4220_hw4_pb2.IDKey(node = meNode, idKey = requestedId)
+		idKey = csci4220_hw4_pb2.IDKey(node = meNode, idkey = requestedId)
 		try:
 			stub = csci4220_hw4_pb2_grpc.KadImplStub(channel)
 			response = stub.FindNode(idKey)
